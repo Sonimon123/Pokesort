20daede08ce9901574ffe922d6928287874060cd
Sonimon123 <jrkargbo@aol.com>
2022-08-22 00:20:37 -0400

    Fixed Initial Batch

--- src/js/data/2022-08-21.js
+++ src/js/data/2022-08-21.js
@@ -62,295 +62,295 @@ dataSet[dataSetVersion].characterData = [
 	  origin: ["game"],
 	  child: false
     }
-  }//,
-  // {
-    // name: "Professor Turo",
-    // img: "351px-Violet_Turo.png",
-    // opts: {
-      // gender: ["male"],
-      // group: ["prof"],
-	  // origin: ["game"],
-	  // child: false
-    // }
-  // },
-  // {
-    // name: "Spark",
-    // img: "390px-GO_Spark.png",
-    // opts: {
-      // gender: ["male"],
-      // group: ["prof"],
-	  // origin: ["spin"],
-	  // child: false
-    // },
-  // },
-  // {
-    // name: "Professor Laventon",
-    // img: "402px-Legends_Arceus_Professor_Laventon.png",
-    // opts: {
-      // gender: ["male"],
-      // group: ["prof"],
-	  // origin: ["game"],
-	  // child: false
-    // }
-  // },
-  // {
-    // name: "Professor Sada",
-    // img: "409px-Scarlet_Sada.png",
-    // opts: {
-      // gender: ["female"],
-      // group: ["prof"],
-	  // origin: ["game"],
-	  // child: false
-    // }
-  // },
-  // {
-    // name: "Candela",
-    // img: "485px-GO_Candela.png",
-    // opts: {
-      // gender: ["female"],
-      // group: ["prof"],
-	  // origin: ["spin"],
-	  // child: false
-    // }
-  // },
-  // {
-    // name: "Cedric Juniper",
-    // img: "Black_White_Cedric_Juniper.png",
-    // opts: {
-      // gender: ["male"],
-      // group: ["prof"],
-	  // origin: ["game"],
-	  // child: false
-    // }
-  // },
-  // {
-    // name: "Fennel",
-    // img: "Black_White_Fennel.png",
-    // opts: {
-      // gender: ["female"],
-      // group: ["prof"],
-	  // origin: ["game"],
-	  // child: false
-    // }
-  // },
-  // {
-    // name: "Professor Juniper",
-    // img: "Black_White_Juniper.png",
-    // opts: {
-      // gender: ["female"],
-      // group: ["prof"],
-	  // origin: ["game"],
-	  // child: false
-    // }
-  // },
-  // {
-    // name: "Professor Burnet (Dream Radar)",
-    // img: "Burnet_Dream_Radar.png",
-    // opts: {
-      // gender: ["female"],
-      // group: ["prof"],
-	  // origin: ["spin"],
-	  // child: false
-    // }
-  // },
-  // {
-    // name: "Professor Rowan",
-    // img: "Diamond_Pearl_Rowan.png",
-    // opts: {
-      // gender: ["male"],
-      // group: ["prof"],
-	  // origin: ["game"],
-	  // child: false
-    // }
-  // },
-  // {
-    // name: "Blanche",
-    // img: "GO_Blanche.png",
-    // opts: {
-      // gender: ["other"],
-      // group: ["prof"],
-	  // origin: ["spin"],
-	  // child: false
-    // }
-  // },
-  // {
-    // name: "Professor Hastings",
-    // img: "Hastings_Ranger_3.png",
-    // opts: {
-      // gender: ["male"],
-      // group: ["prof"],
-	  // origin: ["spin"],
-	  // child: false
-    // }
-  // },
-  // {
-    // name: "Professor Elm",
-    // img: "HeartGold_SoulSilver_Professor_Elm.png",
-    // opts: {
-      // gender: ["male"],
-      // group: ["prof"],
-	  // origin: ["game"],
-	  // child: false
-    // }
-  // },
-  // {
-    // name: "Professor Kaminko",
-    // img: "Kaminko_XD.png",
-    // opts: {
-      // gender: ["male"],
-      // group: ["prof"],
-	  // origin: ["spin"],
-	  // child: false
-    // }
-  // },
-  // {
-    // name: "Professor Krane",
-    // img: "Krane.png",
-    // opts: {
-      // gender: ["male"],
-      // group: ["prof"],
-	  // origin: ["spin"],
-	  // child: false
-    // }
-  // },
-  // {
-    // name: "Leanne",
-    // img: "Leanne_Ranger3.png",
-    // opts: {
-      // gender: ["female"],
-      // group: ["prof"],
-	  // origin: ["spin"],
-	  // child: false
-    // }
-  // },
-  // {
-    // name: "Professor Oak",
-    // img: "Lets_Go_Pikachu_Eevee_Professor_Oak.png",
-    // opts: {
-      // gender: ["male"],
-      // group: ["prof"],
-	  // origin: ["game"],
-	  // child: false
-    // }
-  // },
-  // {
-    // name: "Professor Bellis",
-    // img: "Masters_Professor_Bellis.png",
-    // opts: {
-      // gender: ["female"],
-      // group: ["prof"],
-	  // origin: ["spin"],
-	  // child: false
-    // }
-  // },
-  // {
-    // name: "Professor Birch",
-    // img: "Omega_Ruby_Alpha_Sapphire_Professor_Birch.png",
-    // opts: {
-      // gender: ["male"],
-      // group: ["prof"],
-	  // origin: ["game"],
-	  // child: false
-    // }
-  // },
-  // {
-    // name: "Professor Ivy",
-    // img: "Professor_Ivy.png",
-    // opts: {
-      // gender: ["female"],
-      // group: ["prof"],
-	  // origin: ["anime"],
-	  // child: false
-    // }
-  // },
-  // {
-    // name: "Professor Mirror",
-    // img: "Professor_Mirror.png",
-    // opts: {
-      // gender: ["male"],
-      // group: ["prof"],
-	  // origin: ["spin"],
-	  // child: false
-    // }
-  // },
-  // {
-    // name: "Professor P",
-    // img: "Professor_P_Trozei.png",
-    // opts: {
-      // gender: ["male"],
-      // group: ["prof"],
-	  // origin: ["spin"],
-	  // child: false
-    // }
-  // },
-  // {
-    // name: "Samson Oak",
-    // img: "Samson_Oak_SM.png",
-    // opts: {
-      // gender: ["male"],
-      // group: ["prof"],
-	  // origin: ["game"],
-	  // child: false
-    // }
-  // },
-  // {
-    // name: "Professor Burnet (Sun/Moon)",
-    // img: "Sun_Moon_Professor_Burnet.png",
-    // opts: {
-      // gender: ["female"],
-      // group: ["prof"],
-	  // origin: ["game"],
-	  // child: false
-    // }
-  // },
-  // {
-    // name: "Professor Kukui",
-    // img: "Sun_Moon_Professor_Kukui.png",
-    // opts: {
-      // gender: ["female"],
-      // group: ["prof"],
-	  // origin: ["game"],
-	  // child: false
-    // }
-  // },
-  // {
-    // name: "Professor Magnolia",
-    // img: "Sword_Shield_Professor_Magnolia.png",
-    // opts: {
-      // gender: ["female"],
-      // group: ["prof"],
-	  // origin: ["game"],
-	  // child: false
-    // }
-  // },
-  // {
-    // name: "Sonia",
-    // img: "Sword_Shield_Sonia.png",
-    // opts: {
-      // gender: ["female"],
-      // group: ["prof"],
-	  // origin: ["game"],
-	  // child: false
-    // }
-  // },
-  // {
-    // name: "Professor Phorus",
-    // img: "UNITE_Professor_Phorus.png",
-    // opts: {
-      // gender: ["female"],
-      // group: ["prof"],
-	  // origin: ["game"],
-	  // child: false
-    // }
-  // },
-  // {
-    // name: "Professor Sycamore",
-    // img: "XY_Professor_Sycamore.png",
-    // opts: {
-      // gender: ["male"],
-      // group: ["prof"],
-	  // origin: ["game"],
-	  // child: false
-    // }
-  // }
+  },
+  {
+    name: "Professor Turo",
+    img: "351px-Violet_Turo.png",
+    opts: {
+      gender: ["male"],
+      group: ["prof"],
+	  origin: ["game"],
+	  child: false
+    }
+  },
+  {
+    name: "Spark",
+    img: "390px-GO_Spark.png",
+    opts: {
+      gender: ["male"],
+      group: ["prof"],
+	  origin: ["spin"],
+	  child: false
+    },
+  },
+  {
+    name: "Professor Laventon",
+    img: "402px-Legends_Arceus_Professor_Laventon.png",
+    opts: {
+      gender: ["male"],
+      group: ["prof"],
+	  origin: ["game"],
+	  child: false
+    }
+  },
+  {
+    name: "Professor Sada",
+    img: "409px-Scarlet_Sada.png",
+    opts: {
+      gender: ["female"],
+      group: ["prof"],
+	  origin: ["game"],
+	  child: false
+    }
+  },
+  {
+    name: "Candela",
+    img: "485px-GO_Candela.png",
+    opts: {
+      gender: ["female"],
+      group: ["prof"],
+	  origin: ["spin"],
+	  child: false
+    }
+  },
+  {
+    name: "Cedric Juniper",
+    img: "Black_White_Cedric_Juniper.png",
+    opts: {
+      gender: ["male"],
+      group: ["prof"],
+	  origin: ["game"],
+	  child: false
+    }
+  },
+  {
+    name: "Fennel",
+    img: "Black_White_Fennel.png",
+    opts: {
+      gender: ["female"],
+      group: ["prof"],
+	  origin: ["game"],
+	  child: false
+    }
+  },
+  {
+    name: "Professor Juniper",
+    img: "Black_White_Juniper.png",
+    opts: {
+      gender: ["female"],
+      group: ["prof"],
+	  origin: ["game"],
+	  child: false
+    }
+  },
+  {
+    name: "Professor Burnet (Dream Radar)",
+    img: "Burnet_Dream_Radar.png",
+    opts: {
+      gender: ["female"],
+      group: ["prof"],
+	  origin: ["spin"],
+	  child: false
+    }
+  },
+  {
+    name: "Professor Rowan",
+    img: "Diamond_Pearl_Rowan.png",
+    opts: {
+      gender: ["male"],
+      group: ["prof"],
+	  origin: ["game"],
+	  child: false
+    }
+  },
+  {
+    name: "Blanche",
+    img: "GO_Blanche.png",
+    opts: {
+      gender: ["other"],
+      group: ["prof"],
+	  origin: ["spin"],
+	  child: false
+    }
+  },
+  {
+    name: "Professor Hastings",
+    img: "Hastings_Ranger_3.png",
+    opts: {
+      gender: ["male"],
+      group: ["prof"],
+	  origin: ["spin"],
+	  child: false
+    }
+  },
+  {
+    name: "Professor Elm",
+    img: "HeartGold_SoulSilver_Professor_Elm.png",
+    opts: {
+      gender: ["male"],
+      group: ["prof"],
+	  origin: ["game"],
+	  child: false
+    }
+  },
+  {
+    name: "Professor Kaminko",
+    img: "Kaminko_XD.png",
+    opts: {
+      gender: ["male"],
+      group: ["prof"],
+	  origin: ["spin"],
+	  child: false
+    }
+  },
+  {
+    name: "Professor Krane",
+    img: "Krane.png",
+    opts: {
+      gender: ["male"],
+      group: ["prof"],
+	  origin: ["spin"],
+	  child: false
+    }
+  },
+  {
+    name: "Leanne",
+    img: "Leanne_Ranger3.png",
+    opts: {
+      gender: ["female"],
+      group: ["prof"],
+	  origin: ["spin"],
+	  child: false
+    }
+  },
+  {
+    name: "Professor Oak",
+    img: "Lets_Go_Pikachu_Eevee_Professor_Oak.png",
+    opts: {
+      gender: ["male"],
+      group: ["prof"],
+	  origin: ["game"],
+	  child: false
+    }
+  },
+  {
+    name: "Professor Bellis",
+    img: "Masters_Professor_Bellis.png",
+    opts: {
+      gender: ["female"],
+      group: ["prof"],
+	  origin: ["spin"],
+	  child: false
+    }
+  },
+  {
+    name: "Professor Birch",
+    img: "Omega_Ruby_Alpha_Sapphire_Professor_Birch.png",
+    opts: {
+      gender: ["male"],
+      group: ["prof"],
+	  origin: ["game"],
+	  child: false
+    }
+  },
+  {
+    name: "Professor Ivy",
+    img: "Professor_Ivy.png",
+    opts: {
+      gender: ["female"],
+      group: ["prof"],
+	  origin: ["anime"],
+	  child: false
+    }
+  },
+  {
+    name: "Professor Mirror",
+    img: "Professor_Mirror.png",
+    opts: {
+      gender: ["male"],
+      group: ["prof"],
+	  origin: ["spin"],
+	  child: false
+    }
+  },
+  {
+    name: "Professor P",
+    img: "Professor_P_Trozei.png",
+    opts: {
+      gender: ["male"],
+      group: ["prof"],
+	  origin: ["spin"],
+	  child: false
+    }
+  },
+  {
+    name: "Samson Oak",
+    img: "Samson_Oak_SM.png",
+    opts: {
+      gender: ["male"],
+      group: ["prof"],
+	  origin: ["game"],
+	  child: false
+    }
+  },
+  {
+    name: "Professor Burnet (Sun/Moon)",
+    img: "Sun_Moon_Professor_Burnet.png",
+    opts: {
+      gender: ["female"],
+      group: ["prof"],
+	  origin: ["game"],
+	  child: false
+    }
+  },
+  {
+    name: "Professor Kukui",
+    img: "Sun_Moon_Professor_Kukui.png",
+    opts: {
+      gender: ["female"],
+      group: ["prof"],
+	  origin: ["game"],
+	  child: false
+    }
+  },
+  {
+    name: "Professor Magnolia",
+    img: "Sword_Shield_Professor_Magnolia.png",
+    opts: {
+      gender: ["female"],
+      group: ["prof"],
+	  origin: ["game"],
+	  child: false
+    }
+  },
+  {
+    name: "Sonia",
+    img: "Sword_Shield_Sonia.png",
+    opts: {
+      gender: ["female"],
+      group: ["prof"],
+	  origin: ["game"],
+	  child: false
+    }
+  },
+  {
+    name: "Professor Phorus",
+    img: "UNITE_Professor_Phorus.png",
+    opts: {
+      gender: ["female"],
+      group: ["prof"],
+	  origin: ["game"],
+	  child: false
+    }
+  },
+  {
+    name: "Professor Sycamore",
+    img: "XY_Professor_Sycamore.png",
+    opts: {
+      gender: ["male"],
+      group: ["prof"],
+	  origin: ["game"],
+	  child: false
+    }
+  }
 ];
